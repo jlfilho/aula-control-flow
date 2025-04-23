ca9d1c48d333785b003cc384d623f637b9199b67
João da Mata <jlfilho@users.noreply.github.com>
2025-04-23 19:44:19 -0400

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ControlflowComponent } from './controlflow/controlflow.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ControlflowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
